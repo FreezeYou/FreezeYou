f67f883aeb722cc108a00e8b83ff5425b46ced21
Playhi <000902play@gmail.com>
2017-09-04 21:35:49 +0800

    Initial commit

---
+++ app/src/main/java/cf/playhi/freezeyou/Main.java
@@ -0,0 +1,11 @@
+package cf.playhi.freezeyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
