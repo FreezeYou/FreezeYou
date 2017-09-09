cc1c11d8bfafd417bb54a645b04ba01990cfd540
Playhi <000902play@gmail.com>
2017-09-09 21:02:01 +0800

    Upload

--- app/src/main/java/cf/playhi/freezeyou/Freeze.java
+++ app/src/main/java/cf/playhi/freezeyou/Freeze.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
 
 public class Freeze extends Activity{
 
@@ -23,22 +22,7 @@ public class Freeze extends Activity{
         pkgName = getIntent().getStringExtra("pkgName");
         if (pkgName.equals("unknown")){
             Toast.makeText(getApplicationContext(),"参数错误",Toast.LENGTH_LONG).show();
-            finish();
-        }
-        try {
-            process = Runtime.getRuntime().exec("su");
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(process.getInputStream()));
-//            String line = null;
-//            while ((line = in.readLine()) != null) {
-//                backData += line + "\n";
-//            }
-            Toast.makeText(getApplicationContext(),process.getInputStream().toString(),Toast.LENGTH_LONG).show();
-        } catch (IOException e){
-            Log.e("Error",e.getMessage());
-            Toast.makeText(getApplicationContext(),"获取ROOT权限时出错",Toast.LENGTH_LONG).show();
-            destroyProcess();
-            finish();
+            Freeze.this.finish();
         }
         makeDialog("提示","请选择具体操作");
     }
@@ -52,27 +36,26 @@ public class Freeze extends Activity{
                     public void onClick(DialogInterface dialogInterface, int i) {
                         if (backData.equals("backData")){
                             try {
+                                process = Runtime.getRuntime().exec("su");
+//            BufferedReader in = new BufferedReader(
+//                    new InputStreamReader(process.getInputStream()));
+//            String line = null;
+//            while ((line = in.readLine()) != null) {
+//                backData += line + "\n";
+//            }
+                                Toast.makeText(getApplicationContext(),process.getInputStream().toString(),Toast.LENGTH_LONG).show();
                                 outputStream = new DataOutputStream(process.getOutputStream());
                                 outputStream.writeBytes("pm disable " + pkgName + "\n");
                                 outputStream.writeBytes("exit\n");
                                 outputStream.flush();
-                            } catch (IOException e){
-                                Log.e("Error",e.getMessage());
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                                destroyProcess();
-                                finish();
-                            }
-                            try {
                                 process.waitFor();
-                            }
-                            catch (InterruptedException e){
+                            } catch (Exception e){
                                 Log.e("Error",e.getMessage());
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                                destroyProcess();
-                                finish();
+                                Toast.makeText(getApplicationContext(),"异常 "+e.getMessage(),Toast.LENGTH_LONG).show();
+                                destroyProcess(true);
                             }
                             Toast.makeText(getApplicationContext(),"执行完成",Toast.LENGTH_LONG).show();
-                            destroyProcess();
+                            destroyProcess(true);
                         }
                     }
                 })
@@ -81,48 +64,52 @@ public class Freeze extends Activity{
                     public void onClick(DialogInterface dialogInterface, int i) {
                         if (backData.equals("backData")){
                             try {
+                                process = Runtime.getRuntime().exec("su");
+                                Toast.makeText(getApplicationContext(),process.getInputStream().toString(),Toast.LENGTH_LONG).show();
                                 outputStream = new DataOutputStream(process.getOutputStream());
                                 outputStream.writeBytes("pm enable " + pkgName + "\n");
                                 outputStream.writeBytes("exit\n");
                                 outputStream.flush();
-                            } catch (IOException e){
-                                Log.e("Error",e.getMessage());
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                                destroyProcess();
-                                finish();
-                            }
-                            try {
                                 process.waitFor();
-                            }
-                            catch (InterruptedException e){
+                            } catch (Exception e){
                                 Log.e("Error",e.getMessage());
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                                destroyProcess();
-                                finish();
+                                Toast.makeText(getApplicationContext(),"异常 "+e.getMessage(),Toast.LENGTH_LONG).show();
+                                destroyProcess(true);
                             }
                             Toast.makeText(getApplicationContext(),"执行完成",Toast.LENGTH_LONG).show();
-                            destroyProcess();
+                            destroyProcess(true);
                         }
                     }
                 })
                 .setNeutralButton("取消", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        destroyProcess();
+                        destroyProcess(true);
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        destroyProcess(true);
                     }
                 })
                 .create();
         alertDialog.show();
     }
 
-    private void destroyProcess(){
+    private void destroyProcess(Boolean finish){
         try {
             if (outputStream != null) {
                 outputStream.close();
             }
             process.destroy();
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(),"退出权限时出错",Toast.LENGTH_LONG).show();
+            if (finish){
+                Freeze.this.finish();
+            }
+        } catch (Exception e) {
+            if (finish){
+                Freeze.this.finish();
+            }
         }
     }
 }
