e3a986b01d2c92a011e89c48ed0b060a4b74c52f
Playhi <000902play@gmail.com>
2017-09-15 22:04:38 +0800

    根据包名排序&加载中

--- app/src/main/java/cf/playhi/freezeyou/Main.java
+++ app/src/main/java/cf/playhi/freezeyou/Main.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -12,60 +13,92 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//com.ibm.icu.text.Collator
 
 public class Main extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        ListView app_listView = findViewById(R.id.app_list);
-        final List<Map<String, String>> AppList = new ArrayList<Map<String, String>>();
-        List<ApplicationInfo> applicationInfo = getApplicationContext().getPackageManager().getInstalledApplications(0);
-        int size = applicationInfo.size();
-        for(int i=0;i<size;i++){
-            String name = getPackageManager().getApplicationLabel(applicationInfo.get(i)).toString();
-            String packageName = applicationInfo.get(i).packageName;
-            Map<String, String> keyValuePair = new HashMap<String, String>();
-            keyValuePair.put("Name", name);
-            keyValuePair.put("PackageName", packageName);
-            AppList.add(keyValuePair);
-        }
-
-        ListAdapter adapter = new SimpleAdapter(this, AppList,
-                android.R.layout.simple_list_item_2, new String[] { "Name",
-                "PackageName" }, new int[] { android.R.id.text1,android.R.id.text2 });
-        app_listView.setAdapter(adapter);
-
-        final List<ApplicationInfo> applicationInfoF = applicationInfo;
-        app_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onItemClick(final AdapterView<?> adapterView, View view, final int i, final long l) {
-                AlertDialog alertDialog = new AlertDialog.Builder(Main.this)
-                        .setTitle("提示")
-                        .setMessage("是否在桌面创建冻结/解冻 "+ getPackageManager().getApplicationLabel(applicationInfoF.get(i)).toString() +" 的快捷方式？")
-                        .setNegativeButton("否", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
+            public void run() {
 
-                            }
-                        })
-                        .setPositiveButton("是", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int ii) {
-                                createShortCut(getPackageManager().getApplicationLabel(applicationInfoF.get(i)).toString(),
-                                        applicationInfoF.get(i).packageName,getPackageManager().getApplicationIcon(applicationInfoF.get(i)));
-                            }
-                        })
-                        .create();
-                alertDialog.show();
+                final ListView app_listView = findViewById(R.id.app_list);
+                final ProgressBar progressBar = findViewById(R.id.progressBar);
+                final List<Map<String, String>> AppList = new ArrayList<Map<String, String>>();
+                List<ApplicationInfo> applicationInfo = getApplicationContext().getPackageManager().getInstalledApplications(0);
+                int size = applicationInfo.size();
+                for(int i=0;i<size;i++){
+                    String name = getPackageManager().getApplicationLabel(applicationInfo.get(i)).toString();
+                    String packageName = applicationInfo.get(i).packageName;
+                    Map<String, String> keyValuePair = new HashMap<String, String>();
+                    keyValuePair.put("Name", name);
+                    keyValuePair.put("PackageName", packageName);
+                    AppList.add(keyValuePair);
+                }
+
+                if (!AppList.isEmpty()) {
+                    Collections.sort(AppList, new Comparator<Map<String,String>>() {
+                        @Override
+                        public int compare(Map<String, String> stringStringMap, Map<String, String> t1) {
+                            return ((String) stringStringMap.get("PackageName")).compareTo((String) t1.get("PackageName"));
+                        }
+                    });
+                }
+
+                final ListAdapter adapter = new SimpleAdapter(Main.this, AppList,
+                        android.R.layout.simple_list_item_2, new String[] { "Name",
+                        "PackageName" }, new int[] { android.R.id.text1,android.R.id.text2 });
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                        app_listView.setAdapter(adapter);
+                    }
+                });
+
+                app_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(final AdapterView<?> adapterView, View view, final int i, final long l) {
+                        HashMap<String,String> map=(HashMap<String,String>)app_listView.getItemAtPosition(i);
+                        final String name=map.get("Name");
+                        final String pkgName=map.get("PackageName");
+                        AlertDialog alertDialog = new AlertDialog.Builder(Main.this)
+                                .setTitle("提示")
+                                .setMessage("是否在桌面创建冻结/解冻 "+ name +" 的快捷方式？")
+                                .setNegativeButton("否", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                })
+                                .setPositiveButton("是", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int ii) {
+                                        try{
+                                            createShortCut(name,pkgName,getPackageManager().getApplicationIcon(pkgName));
+                                        }catch (PackageManager.NameNotFoundException e){
+                                            Toast.makeText(getApplicationContext(),"未找到该应用程序",Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                })
+                                .create();
+                        alertDialog.show();
+                    }
+                });
             }
-        });
+        }).start();
     }
 
     public void createShortCut(String title, String pkgName, Drawable icon){
@@ -78,5 +111,6 @@ public class Main extends Activity {
         addShortCut.putExtra(Intent.EXTRA_SHORTCUT_ICON,bd.getBitmap());
         addShortCut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
         sendBroadcast(addShortCut);
+        Toast.makeText(getApplicationContext(),"已发出创建请求",Toast.LENGTH_SHORT).show();
     }
 }
