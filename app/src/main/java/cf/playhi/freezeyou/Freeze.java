14a924a39e7670e40e63f9f0ce8036891140f1fd
Playhi <000902play@gmail.com>
2017-09-23 21:13:39 +0800

    优化用户体验+BUG修复

--- app/src/main/java/cf/playhi/freezeyou/Freeze.java
+++ app/src/main/java/cf/playhi/freezeyou/Freeze.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import java.io.DataOutputStream;
@@ -25,7 +24,21 @@ public class Freeze extends Activity{
             Toast.makeText(getApplicationContext(),"参数错误",Toast.LENGTH_LONG).show();
             Freeze.this.finish();
         }
-        makeDialog("提示","请选择具体操作");
+        if (getPackageManager().getLaunchIntentForPackage(pkgName)!=null){
+            try{
+                makeDialog2(getPackageManager().getApplicationLabel(getPackageManager().getApplicationInfo(pkgName,0)).toString(),"请选择具体操作");
+            }catch (Exception e){
+                e.printStackTrace();
+                makeDialog2("提示","请选择具体操作");
+            }
+        } else {
+            try{
+                makeDialog(getPackageManager().getApplicationLabel(getPackageManager().getApplicationInfo(pkgName,0)).toString(),"请选择具体操作");
+            }catch (Exception e){
+                e.printStackTrace();
+                makeDialog("提示","请选择具体操作");
+            }
+        }
     }
 
     private void makeDialog(String title,String message){
@@ -38,19 +51,25 @@ public class Freeze extends Activity{
                         if (backData.equals("backData")){
                             try {
                                 process = Runtime.getRuntime().exec("su");
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(process.getInputStream()));
-//            String line = null;
-//            while ((line = in.readLine()) != null) {
-//                backData += line + "\n";
-//            }
+//                                BufferedReader in = new BufferedReader(
+//                                        new InputStreamReader(process.getInputStream()));
+//                                String line = in.readLine();
+//                                while (line!=null) {
+//                                    while(line.contains("Permission denied")){
+//                                        Toast.makeText(getApplicationContext(),"该功能需要ROOT权限",Toast.LENGTH_LONG).show();
+//                                        destroyProcess(false);
+//                                        makeDialog("提示","请选择具体操作");
+//                                    }
+//                                }
+//                                in.close();
+//
                                 outputStream = new DataOutputStream(process.getOutputStream());
                                 outputStream.writeBytes("pm disable " + pkgName + "\n");
                                 outputStream.writeBytes("exit\n");
                                 outputStream.flush();
                                 process.waitFor();
                             } catch (Exception e){
-                                Log.e("Error",e.getMessage());
+                                e.printStackTrace();
                                 Toast.makeText(getApplicationContext(),"异常 "+e.getMessage(),Toast.LENGTH_LONG).show();
                                 destroyProcess(true);
                             }
@@ -71,15 +90,13 @@ public class Freeze extends Activity{
                                 outputStream.flush();
                                 process.waitFor();
                             } catch (Exception e){
-                                Log.e("Error",e.getMessage());
+                                e.printStackTrace();
                                 Toast.makeText(getApplicationContext(),"异常 "+e.getMessage(),Toast.LENGTH_LONG).show();
                                 destroyProcess(true);
                             }
-//                            Toast.makeText(getApplicationContext(),"执行完成",Toast.LENGTH_LONG).show();
-//                            destroyProcess(true);
                             AlertDialog alertDialog = new AlertDialog.Builder(Freeze.this)
                                     .setTitle("提示")
-                                    .setMessage("已解冻，立即启动？")
+                                    .setMessage("已要求解冻，立即尝试启动？")
                                     .setNegativeButton("否", new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialogInterface, int i) {
@@ -89,14 +106,82 @@ public class Freeze extends Activity{
                                     .setPositiveButton("是", new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialogInterface, int ii) {
-                                            startActivity(new Intent(
-                                                    getPackageManager().getLaunchIntentForPackage(pkgName)
-                                            ));
+                                            if (getPackageManager().getLaunchIntentForPackage(pkgName)!=null){
+                                                Intent intent = new Intent(
+                                                        getPackageManager().getLaunchIntentForPackage(pkgName));
+                                                startActivity(intent);
+                                                destroyProcess(true);
+                                            } else {
+                                                Toast.makeText(getApplicationContext(),
+                                                        "未找到程序入口或由于未获得ROOT权限导致解冻失败",
+                                                        Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    })
+                                    .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                        @Override
+                                        public void onCancel(DialogInterface dialogInterface) {
                                             destroyProcess(true);
                                         }
                                     })
                                     .create();
                             alertDialog.show();
+                        }
+                    }
+                })
+                .setNeutralButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        destroyProcess(true);
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        destroyProcess(true);
+                    }
+                })
+                .create();
+        alertDialog.show();
+    }
+
+    private void makeDialog2(String title,String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(Freeze.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setNegativeButton("冻结", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (backData.equals("backData")){
+                            try {
+                                process = Runtime.getRuntime().exec("su");
+                                outputStream = new DataOutputStream(process.getOutputStream());
+                                outputStream.writeBytes("pm disable " + pkgName + "\n");
+                                outputStream.writeBytes("exit\n");
+                                outputStream.flush();
+                                process.waitFor();
+                            } catch (Exception e){
+                                e.printStackTrace();
+                                Toast.makeText(getApplicationContext(),"异常 "+e.getMessage(),Toast.LENGTH_LONG).show();
+                                destroyProcess(true);
+                            }
+                            Toast.makeText(getApplicationContext(),"执行完成",Toast.LENGTH_LONG).show();
+                            destroyProcess(true);
+                        }
+                    }
+                })
+                .setPositiveButton("启动", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (getPackageManager().getLaunchIntentForPackage(pkgName)!=null){
+                            Intent intent = new Intent(
+                                    getPackageManager().getLaunchIntentForPackage(pkgName));
+                            startActivity(intent);
+                            destroyProcess(true);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "未找到程序入口",
+                                    Toast.LENGTH_LONG).show();
                         }
                     }
                 })
