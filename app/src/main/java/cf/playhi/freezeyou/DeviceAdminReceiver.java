89b1cd0ca474d45b7cc5ebe995cdf1d9188e58fa
Playhi <000902play@gmail.com>
2018-02-04 16:33:51 +0800

    Update

---
+++ app/src/main/java/cf/playhi/freezeyou/DeviceAdminReceiver.java
@@ -0,0 +1,28 @@
+package cf.playhi.freezeyou;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
+
+    void showToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        showToast(context, "Enabled");
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+        return "Why?";
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        showToast(context, "Disabled");
+    }
+
+}
