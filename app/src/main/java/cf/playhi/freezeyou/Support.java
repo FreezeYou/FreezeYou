854a29cdff0d01a6a084fa57af03c3d4d428c490
Playhi <000902play@gmail.com>
2018-07-04 20:15:09 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/Support.java
+++ app/src/main/java/cf/playhi/freezeyou/Support.java
@@ -21,7 +21,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
