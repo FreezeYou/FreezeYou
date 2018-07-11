a4a6de4484d1bd7ac7b845748d54ee46bf10249f
Playhi <000902play@gmail.com>
2018-07-11 21:27:15 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/SelectOperation.java
+++ app/src/main/java/cf/playhi/freezeyou/SelectOperation.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
