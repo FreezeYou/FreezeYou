1f2d36d941ce9116dfbe8fbecbcf8568f3ae1567
Playhi <000902play@gmail.com>
2018-08-07 14:04:44 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/UninstallReceiver.java
+++ app/src/main/java/cf/playhi/freezeyou/UninstallReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.io.File;
+
 import static cf.playhi.freezeyou.Support.removeFromOneKeyList;
 
 public class UninstallReceiver extends BroadcastReceiver {
@@ -16,6 +18,13 @@ public class UninstallReceiver extends BroadcastReceiver {
                 pkgName = pkgName.replace("package:", "");
                 removeFromOneKeyList(context, "AutoFreezeApplicationList", pkgName);
                 removeFromOneKeyList(context, "OneKeyUFApplicationList", pkgName);
+                //清理被卸载应用程序的图标数据
+                File file = new File(context.getFilesDir() + "/icon/" + pkgName + ".png");
+                if (file.exists()&&file.isFile()) {
+                    file.delete();
+                }
+                //清理被卸载应用程序的名称
+                context.getSharedPreferences("NameOfPackages", Context.MODE_PRIVATE).edit().remove(pkgName).apply();
             }
         }
     }
