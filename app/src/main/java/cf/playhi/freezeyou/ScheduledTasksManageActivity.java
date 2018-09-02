fb543f5ec273187bfcaf83b2e6b269ac1482ebd2
Playhi <000902play@gmail.com>
2018-09-02 21:02:25 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/ScheduledTasksManageActivity.java
+++ app/src/main/java/cf/playhi/freezeyou/ScheduledTasksManageActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
