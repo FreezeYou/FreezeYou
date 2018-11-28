85c03273282d411b3e4513bd54c8cbfa76770690
Playhi <000902play@gmail.com>
2018-11-28 18:32:03 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/AutoDiagnosisActivity.java
+++ app/src/main/java/cf/playhi/freezeyou/AutoDiagnosisActivity.java
@@ -55,7 +55,7 @@ public class AutoDiagnosisActivity extends Activity {
         }
     }
 
-    protected void go() {
+    private void go() {
         ListView adg_listView = findViewById(R.id.adg_listView);
         ProgressBar adg_progressBar = findViewById(R.id.adg_progressBar);
         List<Map<String, String>> problemsList = new ArrayList<>();
@@ -85,8 +85,11 @@ public class AutoDiagnosisActivity extends Activity {
         checkRootPermission(problemsList);
         setProgress(adg_progressBar, 70);
 
-        checkIsIgnoringBatteryOptimizations(problemsList);
+        checkIsPowerSaveMode(problemsList);
         setProgress(adg_progressBar, 80);
+
+        checkIsIgnoringBatteryOptimizations(problemsList);
+        setProgress(adg_progressBar, 85);
 
         checkIfNoProblemFound(problemsList);
         setProgress(adg_progressBar, 90);
@@ -103,7 +106,7 @@ public class AutoDiagnosisActivity extends Activity {
         done(adg_progressBar, adg_listView, adapter);
     }
 
-    protected void disableIndeterminate(final ProgressBar progressBar) {
+    private void disableIndeterminate(final ProgressBar progressBar) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -112,7 +115,7 @@ public class AutoDiagnosisActivity extends Activity {
         });
     }
 
-    protected void done(final ProgressBar progressBar, final ListView listView, final SimpleAdapter adapter) {
+    private void done(final ProgressBar progressBar, final ListView listView, final SimpleAdapter adapter) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -122,7 +125,7 @@ public class AutoDiagnosisActivity extends Activity {
         });
     }
 
-    protected void setProgress(final ProgressBar progressBar, final int progress) {
+    private void setProgress(final ProgressBar progressBar, final int progress) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -135,14 +138,14 @@ public class AutoDiagnosisActivity extends Activity {
         });
     }
 
-    protected HashMap<String, String> generateHashMap(String title, String sTitle) {
+    private HashMap<String, String> generateHashMap(String title, String sTitle) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("title", title);
         hashMap.put("sTitle", sTitle);
         return hashMap;
     }
 
-    protected void checkSystemVersion(List<Map<String, String>> problemsList) {
+    private void checkSystemVersion(List<Map<String, String>> problemsList) {
         if (Build.VERSION.SDK_INT < 21) {
             problemsList.add(
                     generateHashMap(getString(R.string.sysVerLow), getString(R.string.someFuncUn))
@@ -150,7 +153,7 @@ public class AutoDiagnosisActivity extends Activity {
         }
     }
 
-    protected void checkAccessibilityService(List<Map<String, String>> problemsList, AppPreferences appPreferences) {
+    private void checkAccessibilityService(List<Map<String, String>> problemsList, AppPreferences appPreferences) {
         if ((getDatabasePath("scheduledTriggerTasks").exists() || appPreferences.getBoolean("freezeOnceQuit", false) || appPreferences.getBoolean("avoidFreezeForegroundApplications", false)) && !isAccessibilitySettingsOn(this)) {
             problemsList.add(
                     generateHashMap(getString(R.string.ACBSNotEnabled), getString(R.string.affect) + " " + getString(R.string.avoidFreezeForegroundApplications) + " " + getString(R.string.scheduledTasks) + " " + getString(R.string.etc))
@@ -158,7 +161,7 @@ public class AutoDiagnosisActivity extends Activity {
         }
     }
 
-    protected void checkNotificationListenerPermission(List<Map<String, String>> problemsList, AppPreferences appPreferences) {
+    private void checkNotificationListenerPermission(List<Map<String, String>> problemsList, AppPreferences appPreferences) {
         if (appPreferences.getBoolean("avoidFreezeNotifyingApplications", false)) {
             String s = Settings.Secure.getString(getContentResolver(), "enabled_notification_listeners");
             if (s == null || !s.contains("cf.playhi.freezeyou/cf.playhi.freezeyou.MyNotificationListenerService")) {
@@ -169,26 +172,26 @@ public class AutoDiagnosisActivity extends Activity {
         }
     }
 
-    protected void checkNotifyPermission(List<Map<String, String>> problemsList) {
+    private void checkNotifyPermission(List<Map<String, String>> problemsList) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if (notificationManager == null || (Build.VERSION.SDK_INT >= 24 && !notificationManager.areNotificationsEnabled())) {
             problemsList.add(generateHashMap(getString(R.string.noNotifyPermission), getString(R.string.mayCannotNotify)));
         }
     }
 
-    protected void checkIsDeviceOwner(List<Map<String, String>> problemsList) {
+    private void checkIsDeviceOwner(List<Map<String, String>> problemsList) {
         if (!Support.isDeviceOwner(this)) {
             problemsList.add(generateHashMap(getString(R.string.noMRootPermission), getString(R.string.someFuncMayRestrict)));
         }
     }
 
-    protected void checkLongTimeNoUpdate(List<Map<String, String>> problemsList) {
+    private void checkLongTimeNoUpdate(List<Map<String, String>> problemsList) {
         SharedPreferences sharedPreferences = getSharedPreferences("Ver", MODE_PRIVATE);
         if ((new Date().getTime() - sharedPreferences.getLong("Time", 0)) > 1728000000)
             problemsList.add(generateHashMap(getString(R.string.notUpdatedForALongTime), getString(R.string.someNewFuncMayPub)));
     }
 
-    protected void checkRootPermission(List<Map<String, String>> problemsList) {
+    private void checkRootPermission(List<Map<String, String>> problemsList) {
         boolean hasPermission = true;
         int value = -1;
         try {
@@ -208,14 +211,20 @@ public class AutoDiagnosisActivity extends Activity {
         }
     }
 
-    protected void checkIsIgnoringBatteryOptimizations(List<Map<String, String>> problemsList) {
+    private void checkIsIgnoringBatteryOptimizations(List<Map<String, String>> problemsList) {
         if (Build.VERSION.SDK_INT >= 23)
             if (!((PowerManager) getSystemService(Context.POWER_SERVICE)).isIgnoringBatteryOptimizations("cf.playhi.freezeyou")) {
                 problemsList.add(generateHashMap(getString(R.string.noIgnoringBO), getString(R.string.someFuncMayBeAff)));
             }
     }
 
-    protected void checkIfNoProblemFound(List<Map<String, String>> problemsList) {
+    private void checkIsPowerSaveMode(List<Map<String, String>> problemsList) {
+        if (Build.VERSION.SDK_INT >= 21)
+            if (((PowerManager) getSystemService(Context.POWER_SERVICE)).isPowerSaveMode())
+                problemsList.add(generateHashMap(getString(R.string.inPowerSaveMode), getString(R.string.someFuncMayBeAff)));
+    }
+
+    private void checkIfNoProblemFound(List<Map<String, String>> problemsList) {
         if (problemsList.isEmpty()) {
             problemsList.add(generateHashMap(getString(R.string.noProblemsFound), getString(R.string.everySeemsAllRight)));
         }
