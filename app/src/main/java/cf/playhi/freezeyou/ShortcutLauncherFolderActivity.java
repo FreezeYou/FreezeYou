9ffb883d3f36c5a791e50202612950471c23b606
Playhi <000902play@gmail.com>
2019-01-14 21:47:05 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/ShortcutLauncherFolderActivity.java
+++ app/src/main/java/cf/playhi/freezeyou/ShortcutLauncherFolderActivity.java
@@ -2,8 +2,7 @@ package cf.playhi.freezeyou;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -14,8 +13,8 @@ import android.widget.ImageView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import static android.view.Window.FEATURE_NO_TITLE;
 import static cf.playhi.freezeyou.ApplicationIconUtils.getApplicationIcon;
@@ -32,35 +31,55 @@ public class ShortcutLauncherFolderActivity extends Activity {
         requestWindowFeature(FEATURE_NO_TITLE);
         if (Intent.ACTION_CREATE_SHORTCUT.equals(getIntent().getAction())) {
             Intent intent = new Intent();
-            intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(this, ShortcutLauncherFolderActivity.class));
+            intent.putExtra(
+                    Intent.EXTRA_SHORTCUT_INTENT,
+                    new Intent(this, ShortcutLauncherFolderActivity.class)
+                            .putExtra("UUID", Long.toString(new Date().getTime())));
             intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
             intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher_new_round));
             setResult(RESULT_OK, intent);
             finish();
         } else {
             setContentView(R.layout.shortcut_launcher_folder);
+            String uuid = getIntent().getStringExtra("UUID");
             GridView slf_apps_gridView = findViewById(R.id.slf_apps_gridView);
 
             slf_apps_gridView.setColumnWidth((int) (getResources().getDimension(android.R.dimen.app_icon_size) * 1.8));
 //            if (Build.VERSION.SDK_INT>=16)
 //                slf_apps_gridView.setVerticalSpacing(slf_apps_gridView.getRequestedColumnWidth()/slf_apps_gridView.getNumColumns());
 
-            ArrayList<HashMap<String, Object>> folderItems = new ArrayList<>();
+            final ArrayList<HashMap<String, Object>> folderItems = new ArrayList<>();
 
 
-            final List<ApplicationInfo> applicationInfo = getPackageManager().getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
-            int size = applicationInfo == null ? 0 : applicationInfo.size();
-            String pkgName;
-            for (int i = 0; i < size; i++) {
-                HashMap<String, Object> map = new HashMap<>();
-                pkgName = applicationInfo.get(i).packageName;
-                map.put("Icon",
-                        Support.realGetFrozenStatus(this, pkgName, null) ? new BitmapDrawable(getGrayBitmap(getBitmapFromDrawable(getApplicationIcon(this, pkgName, applicationInfo.get(i), false)))) : getApplicationIcon(this, pkgName, applicationInfo.get(i), false)
-                );
-                map.put("Label", getApplicationLabel(this, null, null, pkgName));
+//            final List<ApplicationInfo> applicationInfo = getPackageManager().getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+//            int size = applicationInfo == null ? 0 : applicationInfo.size();
+//            String pkgName;
+            SharedPreferences sharedPreferences = getSharedPreferences(uuid, MODE_PRIVATE);
+            String s = sharedPreferences.getString("pkgS", "");
+            String[] pkgS = s == null ? new String[]{} : s.split(",");
+            for (String aPkg : pkgS) {
+                if (!"".equals(aPkg)) {
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("Icon",
+                            Support.realGetFrozenStatus(this, aPkg, null)
+                                    ?
+                                    new BitmapDrawable(getGrayBitmap(getBitmapFromDrawable(getApplicationIcon(this, aPkg, null, false))))
+                                    :
+                                    getApplicationIcon(this, aPkg, null, false)
+                    );
+                    map.put("Label", getApplicationLabel(this, null, null, aPkg));
+                    map.put("Package", aPkg);
 
-                folderItems.add(map);
+                    folderItems.add(map);
+                }
             }
+
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("Icon", getResources().getDrawable(R.drawable.grid_add));
+            map.put("Label", getString(R.string.add));
+            map.put("Package", "freezeyou@add");
+
+            folderItems.add(map);
 
             SimpleAdapter simpleAdapter = new SimpleAdapter(this, folderItems,
                     R.layout.shortcut_launcher_folder_item, new String[]{"Icon", "Label"},
@@ -83,9 +102,18 @@ public class ShortcutLauncherFolderActivity extends Activity {
             slf_apps_gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Support.checkFrozenStatusAndStartApp(
-                            ShortcutLauncherFolderActivity.this,
-                            applicationInfo.get(position).packageName);
+                    String pkg = (String) folderItems.get(position).get("Package");
+                    if ("freezeyou@add".equals(pkg)) {
+                        startActivityForResult(
+                                new Intent(
+                                        ShortcutLauncherFolderActivity.this,
+                                        FUFLauncherShortcutCreator.class),
+                                7001);
+                    } else {
+                        Support.checkFrozenStatusAndStartApp(
+                                ShortcutLauncherFolderActivity.this,
+                                pkg);
+                    }
                 }
             });
 
