69256cd359dd3489e7b06cb7cfd07ef4d6939006
Playhi <000902play@gmail.com>
2019-01-29 21:19:40 +0800

    Update

---
+++ app/src/main/java/cf/playhi/freezeyou/LauncherShortcutConfirmAndGenerateActivity.java
@@ -0,0 +1,45 @@
+package cf.playhi.freezeyou;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class LauncherShortcutConfirmAndGenerateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ThemeUtils.processSetTheme(this);
+        super.onCreate(savedInstanceState);
+        ThemeUtils.processActionBar(getActionBar());
+        setContentView(R.layout.lscaga_main);
+        init();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void init() {
+        Button lscaga_generate_button = findViewById(R.id.lscaga_generate_button);
+        ImageButton lscaga_icon_imageButton = findViewById(R.id.lscaga_icon_imageButton);
+        TextView lscaga_icon_textView = findViewById(R.id.lscaga_icon_textView);
+        TextView lscaga_displayName_textView = findViewById(R.id.lscaga_displayName_textView);
+        EditText lscaga_displayName_editText = findViewById(R.id.lscaga_displayName_editText);
+
+        if (Build.VERSION.SDK_INT>=23){
+        }
+
+    }
+}
