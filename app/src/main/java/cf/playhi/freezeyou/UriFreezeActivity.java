2fb159af57e73694671e026b290e1022acc659ef
Playhi <000902play@gmail.com>
2019-03-05 22:50:41 +0800

    Update

--- app/src/main/java/cf/playhi/freezeyou/UriFreezeActivity.java
+++ app/src/main/java/cf/playhi/freezeyou/UriFreezeActivity.java
@@ -1,7 +1,13 @@
 package cf.playhi.freezeyou;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class UriFreezeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 }
