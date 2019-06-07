d509015fb76e41f594ebe362647653365b57e1d9
Playhi <000902play@gmail.com>
2019-06-07 21:14:10 +0800

    Update

---
+++ app/src/main/java/cf/playhi/freezeyou/LocaleChangedReceiver.java
@@ -0,0 +1,34 @@
+package cf.playhi.freezeyou;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+public class LocaleChangedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent == null) {
+            return;
+        }
+
+        if (Intent.ACTION_LOCALE_CHANGED.equals(intent.getAction())) {
+            PackageManager packageManager = context.getPackageManager();
+            List<ApplicationInfo> applicationInfo = packageManager.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+            //更新所有应用程序名称
+            for (ApplicationInfo applicationInfo1 : applicationInfo) {
+                context.getSharedPreferences("NameOfPackages", Context.MODE_PRIVATE)
+                        .edit()
+                        .putString(
+                                applicationInfo1.packageName,
+                                context.getPackageManager()
+                                        .getApplicationLabel(applicationInfo1).toString())
+                        .apply();
+            }
+        }
+    }
+}
--- app/src/main/java/cf/playhi/freezeyou/UninstallReceiver.java
+++ app/src/main/java/cf/playhi/freezeyou/UninstallReceiver.java
@@ -12,23 +12,34 @@ import static cf.playhi.freezeyou.OneKeyListUtils.removeFromOneKeyList;
 public class UninstallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (Intent.ACTION_PACKAGE_FULLY_REMOVED.equals(action)) {
+
+        if (intent == null) {
+            return;
+        }
+
+        if (Intent.ACTION_PACKAGE_FULLY_REMOVED.equals(intent.getAction())) {
             String pkgName = intent.getDataString();
             if (pkgName != null) {
                 pkgName = pkgName.replace("package:", "");
-                removeFromOneKeyList(context, context.getString(R.string.sAutoFreezeApplicationList), pkgName);
-                removeFromOneKeyList(context, context.getString(R.string.sOneKeyUFApplicationList), pkgName);
-                removeFromOneKeyList(context, context.getString(R.string.sFreezeOnceQuit), pkgName);
-                //清理被卸载应用程序的图标数据
-                File file = new File(context.getFilesDir() + "/icon/" + pkgName + ".png");
-                if (file.exists() && file.isFile()) {
-                    file.delete();
+                if (ApplicationInfoUtils.getApplicationInfoFromPkgName(pkgName, context) == null) {
+                    removeFromOneKeyList(context, context.getString(R.string.sAutoFreezeApplicationList), pkgName);
+                    removeFromOneKeyList(context, context.getString(R.string.sOneKeyUFApplicationList), pkgName);
+                    removeFromOneKeyList(context, context.getString(R.string.sFreezeOnceQuit), pkgName);
+                    //清理被卸载应用程序的图标数据
+                    File file = new File(context.getFilesDir() + "/icon/" + pkgName + ".png");
+                    if (file.exists() && file.isFile()) {
+                        file.delete();
+                    }
+                    File file2 = new File(context.getCacheDir() + "/icon/" + pkgName + ".png");
+                    if (file2.exists() && file2.isFile()) {
+                        file2.delete();
+                    }
+                    //清理被卸载应用程序的名称
+                    context.getSharedPreferences("NameOfPackages", Context.MODE_PRIVATE)
+                            .edit().remove(pkgName).apply();
+                    //清理可能存在的通知栏提示重新显示数据
+                    deleteNotifying(context, pkgName);
                 }
-                //清理被卸载应用程序的名称
-                context.getSharedPreferences("NameOfPackages", Context.MODE_PRIVATE).edit().remove(pkgName).apply();
-                //清理可能存在的通知栏提示重新显示数据
-                deleteNotifying(context, pkgName);
             }
         }
     }
