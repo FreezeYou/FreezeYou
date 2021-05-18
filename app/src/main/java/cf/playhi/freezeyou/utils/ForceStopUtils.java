7601922a5b22bfb788887915dc683c068719df3a
Playhi <000902play@gmail.com>
2021-05-18 22:15:04 +0800

    Optimize imports

--- app/src/main/java/cf/playhi/freezeyou/utils/ForceStopUtils.java
+++ app/src/main/java/cf/playhi/freezeyou/utils/ForceStopUtils.java
@@ -1,55 +1,15 @@
 package cf.playhi.freezeyou.utils;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.service.notification.StatusBarNotification;
 
 import net.grandcentrix.tray.AppPreferences;
 
 import java.io.DataOutputStream;
 
-import cf.playhi.freezeyou.AskRunActivity;
-import cf.playhi.freezeyou.DeviceAdminReceiver;
-import cf.playhi.freezeyou.FUFService;
 import cf.playhi.freezeyou.MainApplication;
-import cf.playhi.freezeyou.MyNotificationListenerService;
 import cf.playhi.freezeyou.R;
-import cf.playhi.freezeyou.fuf.FUFSinglePackage;
 
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ACTION_MODE_FREEZE;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ACTION_MODE_UNFREEZE;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.API_FREEZEYOU_LEGACY_AUTO;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.API_FREEZEYOU_MROOT_DPM;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.API_FREEZEYOU_ROOT_DISABLE_ENABLE;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.API_FREEZEYOU_ROOT_UNHIDE_HIDE;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_DEVICE_ANDROID_VERSION_TOO_LOW;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_DPM_EXECUTE_FAILED_FROM_SYSTEM;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NOT_DEVICE_POLICY_MANAGER;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NOT_SYSTEM_APP;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NO_ERROR_CAUGHT_UNKNOWN_RESULT;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NO_ERROR_SUCCESS;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NO_ROOT_PERMISSION;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_NO_SUCH_API_MODE;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_OTHER;
-import static cf.playhi.freezeyou.fuf.FUFSinglePackage.ERROR_SINGLE_PACKAGE_NAME_IS_NULL;
-import static cf.playhi.freezeyou.utils.ApplicationIconUtils.getApplicationIcon;
-import static cf.playhi.freezeyou.utils.ApplicationIconUtils.getBitmapFromDrawable;
-import static cf.playhi.freezeyou.utils.ApplicationInfoUtils.getApplicationInfoFromPkgName;
-import static cf.playhi.freezeyou.utils.DevicePolicyManagerUtils.getDevicePolicyManager;
-import static cf.playhi.freezeyou.utils.NotificationUtils.createFUFQuickNotification;
-import static cf.playhi.freezeyou.utils.NotificationUtils.deleteNotification;
 import static cf.playhi.freezeyou.utils.ProcessUtils.destroyProcess;
-import static cf.playhi.freezeyou.utils.ServiceUtils.startService;
-import static cf.playhi.freezeyou.utils.TasksUtils.onFApplications;
-import static cf.playhi.freezeyou.utils.TasksUtils.onUFApplications;
-import static cf.playhi.freezeyou.utils.TasksUtils.runTask;
-import static cf.playhi.freezeyou.utils.ToastUtils.showShortToast;
 import static cf.playhi.freezeyou.utils.ToastUtils.showToast;
 
 public final class ForceStopUtils {
