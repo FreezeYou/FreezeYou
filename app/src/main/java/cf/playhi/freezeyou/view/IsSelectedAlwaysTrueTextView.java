40b9db5569fe6569378103fd3e017a032ac8db34
Playhi <000902play@gmail.com>
2021-09-05 22:06:25 +0800

    Minor update

--- app/src/main/java/cf/playhi/freezeyou/view/IsSelectedAlwaysTrueTextView.java
+++ app/src/main/java/cf/playhi/freezeyou/view/IsSelectedAlwaysTrueTextView.java
@@ -1,27 +1,24 @@
 package cf.playhi.freezeyou.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.TextView;
 
-public class IsSelectedAlwaysTrueTextView extends TextView {
-    public IsSelectedAlwaysTrueTextView(Context context) {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class IsSelectedAlwaysTrueTextView extends AppCompatTextView {
+
+    public IsSelectedAlwaysTrueTextView(@NonNull Context context) {
         super(context);
     }
 
-    public IsSelectedAlwaysTrueTextView(Context context, AttributeSet attrs) {
+    public IsSelectedAlwaysTrueTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public IsSelectedAlwaysTrueTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public IsSelectedAlwaysTrueTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public IsSelectedAlwaysTrueTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
