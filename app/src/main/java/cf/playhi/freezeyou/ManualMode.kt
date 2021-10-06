49adddf8c134ced852e429876dde985331ad4cf6
Playhi <000902play@gmail.com>
2021-10-06 14:23:32 +0800

    Convert ManualMode.java to ManualMode.kt, FUFSinglePackage.java to FUFSinglePackage.kt.

---
+++ app/src/main/java/cf/playhi/freezeyou/ManualMode.kt
@@ -0,0 +1,95 @@
+package cf.playhi.freezeyou
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import cf.playhi.freezeyou.ThemeUtils.processActionBar
+import cf.playhi.freezeyou.ThemeUtils.processSetTheme
+import cf.playhi.freezeyou.app.FreezeYouBaseActivity
+import cf.playhi.freezeyou.fuf.FUFSinglePackage
+import cf.playhi.freezeyou.utils.FUFUtils.preProcessFUFResultAndShowToastAndReturnIfResultBelongsSuccess
+
+class ManualMode : FreezeYouBaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        processSetTheme(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.manualmode)
+        processActionBar(supportActionBar)
+
+        val packageNameEditText = findViewById<EditText>(R.id.manualMode_packageNameEditText)
+        val selectFUFModeButton = findViewById<Button>(R.id.manualMode_selectFUFMode_button)
+        val disableButton = findViewById<Button>(R.id.manualMode_disable_button)
+        val enableButton = findViewById<Button>(R.id.manualMode_enable_button)
+        val context = applicationContext
+        val modeSelections = arrayOf(
+            resources.getStringArray(R.array.selectFUFModeSelection),
+            resources.getStringArray(R.array.selectFUFModeSelectionValues)
+        )
+        packageNameEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.isEmpty()) {
+                    disableButton.isEnabled = false
+                    enableButton.isEnabled = false
+                } else {
+                    disableButton.isEnabled = true
+                    enableButton.isEnabled = true
+                }
+            }
+        })
+        selectFUFModeButton.setOnClickListener {
+            AlertDialog.Builder(this@ManualMode)
+                .setTitle(R.string.selectFUFMode)
+                .setSingleChoiceItems(
+                    modeSelections[0],
+                    selectedModeCheckedPosition
+                ) { dialog: DialogInterface, which: Int ->
+                    selectedModeCheckedPosition = which
+                    selectedMode = modeSelections[1][which].toInt()
+                    selectFUFModeButton.text = modeSelections[0][which]
+                    dialog.dismiss()
+                }
+                .setNegativeButton(R.string.cancel, null)
+                .show()
+        }
+        disableButton.setOnClickListener {
+            processFUFOperation(
+                packageNameEditText.text.toString(),
+                context,
+                true
+            )
+        }
+        enableButton.setOnClickListener {
+            processFUFOperation(
+                packageNameEditText.text.toString(),
+                context,
+                false
+            )
+        }
+    }
+
+    private fun processFUFOperation(pkgName: String, context: Context, freeze: Boolean) {
+        val fufSinglePackage = FUFSinglePackage(context)
+            .setActionMode(
+                if (freeze) FUFSinglePackage.ACTION_MODE_FREEZE else FUFSinglePackage.ACTION_MODE_UNFREEZE
+            )
+            .setAPIMode(selectedMode)
+            .setSinglePackageName(pkgName)
+
+        preProcessFUFResultAndShowToastAndReturnIfResultBelongsSuccess(
+            context, fufSinglePackage.commit(), true
+        )
+    }
+
+    companion object {
+        private var selectedMode = -1
+        private var selectedModeCheckedPosition = -1
+    }
+}
---
+++ app/src/main/java/cf/playhi/freezeyou/fuf/FUFSinglePackage.kt
@@ -0,0 +1,263 @@
+package cf.playhi.freezeyou.fuf
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import cf.playhi.freezeyou.DeviceAdminReceiver.getComponentName
+import cf.playhi.freezeyou.utils.DevicePolicyManagerUtils.getDevicePolicyManager
+import cf.playhi.freezeyou.utils.DevicePolicyManagerUtils.isDeviceOwner
+import cf.playhi.freezeyou.utils.FUFUtils.checkMRootFrozen
+import cf.playhi.freezeyou.utils.FUFUtils.isSystemApp
+import cf.playhi.freezeyou.utils.ProcessUtils.fAURoot
+
+open class FUFSinglePackage {
+
+    private val mContext: Context
+    private var mSinglePackageName: String? = null
+    private var mActionMode = ACTION_MODE_UNFREEZE
+    private var mAPIMode = API_FREEZEYOU_LEGACY_AUTO
+
+    constructor(context: Context) {
+        mContext = context
+    }
+
+    constructor(context: Context, singlePackageName: String?, actionMode: Int, apiMode: Int) {
+        mContext = context
+        mSinglePackageName = singlePackageName
+        mActionMode = actionMode
+        mAPIMode = apiMode
+    }
+
+    fun setSinglePackageName(singlePackageName: String?): FUFSinglePackage {
+        mSinglePackageName = singlePackageName
+        return this
+    }
+
+    fun setActionMode(actionMode: Int): FUFSinglePackage {
+        mActionMode = actionMode
+        return this
+    }
+
+    fun setAPIMode(apiMode: Int): FUFSinglePackage {
+        mAPIMode = apiMode
+        return this
+    }
+
+    fun commit(): Int {
+        return when (mAPIMode) {
+            API_FREEZEYOU_LEGACY_AUTO ->
+                pureExecuteAPIAutoAction()
+            API_FREEZEYOU_MROOT_DPM ->
+                pureExecuteAPIDPMAction()
+            API_FREEZEYOU_ROOT_DISABLE_ENABLE ->
+                pureExecuteAPIRootAction(false)
+            API_FREEZEYOU_ROOT_UNHIDE_HIDE ->
+                pureExecuteAPIRootAction(true)
+            API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE ->
+                pureExecuteAPISystemAppDisabledAction()
+            API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE_UNTIL_USED ->
+                pureExecuteAPISystemAppDisabledUntilUsedAction()
+            API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE_USER ->
+                pureExecuteAPISystemAppDisabledUserAction()
+            else ->
+                ERROR_NO_SUCH_API_MODE
+        }
+    }
+
+    private fun pureExecuteAPIAutoAction(): Int {
+        return if (mActionMode == ACTION_MODE_FREEZE) {
+            if (Build.VERSION.SDK_INT >= 21 && isDeviceOwner(mContext)) {
+                pureExecuteAPIDPMAction()
+            } else {
+                pureExecuteAPIRootAction()
+            }
+        } else {
+            if (checkMRootFrozen(mContext, mSinglePackageName)) {
+                pureExecuteAPIDPMAction()
+            } else {
+                pureExecuteAPIRootAction()
+            }
+        }
+    }
+
+    private fun pureExecuteAPIDPMAction(): Int {
+        if (mSinglePackageName == null) return ERROR_SINGLE_PACKAGE_NAME_IS_NULL
+        if (Build.VERSION.SDK_INT < 21) return ERROR_DEVICE_ANDROID_VERSION_TOO_LOW
+        if (!isDeviceOwner(mContext)) return ERROR_NOT_DEVICE_POLICY_MANAGER
+        var returnValue = ERROR_OTHER
+        val hidden = mActionMode == ACTION_MODE_FREEZE
+        if (!hidden &&
+            !getDevicePolicyManager(mContext)
+                .isApplicationHidden(
+                    getComponentName(mContext),
+                    mSinglePackageName
+                )
+        ) {
+            returnValue = ERROR_NO_ERROR_SUCCESS
+        } else {
+            if ("cf.playhi.freezeyou" != mSinglePackageName) {
+                returnValue =
+                    if (getDevicePolicyManager(mContext).setApplicationHidden(
+                            getComponentName(mContext),
+                            mSinglePackageName,
+                            hidden
+                        )
+                    ) {
+                        ERROR_NO_ERROR_SUCCESS
+                    } else {
+                        ERROR_DPM_EXECUTE_FAILED_FROM_SYSTEM
+                    }
+            }
+        }
+        return returnValue
+    }
+
+    private fun pureExecuteAPIRootAction(): Int {
+        return when (mAPIMode) {
+            API_FREEZEYOU_LEGACY_AUTO, API_FREEZEYOU_ROOT_DISABLE_ENABLE ->
+                pureExecuteAPIRootAction(false)
+            API_FREEZEYOU_ROOT_UNHIDE_HIDE ->
+                pureExecuteAPIRootAction(true)
+            else ->
+                ERROR_OTHER
+        }
+    }
+
+    private fun pureExecuteAPIRootAction(hideMode: Boolean): Int {
+        if (mSinglePackageName == null) return ERROR_SINGLE_PACKAGE_NAME_IS_NULL
+        var returnValue = ERROR_OTHER
+        val enable = mActionMode == ACTION_MODE_UNFREEZE
+        if ("cf.playhi.freezeyou" != mSinglePackageName) {
+            try {
+                val exitValue = fAURoot(mSinglePackageName, enable, hideMode)
+                returnValue = if (exitValue == 0) {
+                    ERROR_NO_ERROR_SUCCESS
+                } else {
+                    ERROR_OTHER
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                val eMsg = e.message
+                if (eMsg != null &&
+                    (eMsg.toLowerCase().contains("permission denied")
+                            || eMsg.toLowerCase().contains("not found"))
+                ) {
+                    returnValue = ERROR_NO_ROOT_PERMISSION
+                }
+            }
+        }
+        return returnValue
+    }
+
+    private fun pureExecuteAPISystemAppDisabledUntilUsedAction(): Int {
+        if (mSinglePackageName == null) return ERROR_SINGLE_PACKAGE_NAME_IS_NULL
+        if (Build.VERSION.SDK_INT < 18) return ERROR_DEVICE_ANDROID_VERSION_TOO_LOW
+        if (!isSystemApp(mContext)) return ERROR_NOT_SYSTEM_APP
+        var returnValue = ERROR_OTHER
+        val freeze = mActionMode == ACTION_MODE_FREEZE
+        if ("cf.playhi.freezeyou" != mSinglePackageName) {
+            mContext.packageManager.setApplicationEnabledSetting(
+                mSinglePackageName!!,
+                if (freeze)
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED_UNTIL_USED
+                else
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                0
+            )
+            returnValue = ERROR_NO_ERROR_CAUGHT_UNKNOWN_RESULT
+        }
+        return returnValue
+    }
+
+    private fun pureExecuteAPISystemAppDisabledUserAction(): Int {
+        if (mSinglePackageName == null) return ERROR_SINGLE_PACKAGE_NAME_IS_NULL
+        if (!isSystemApp(mContext)) return ERROR_NOT_SYSTEM_APP
+        var returnValue = ERROR_OTHER
+        val freeze = mActionMode == ACTION_MODE_FREEZE
+        if ("cf.playhi.freezeyou" != mSinglePackageName) {
+            mContext.packageManager.setApplicationEnabledSetting(
+                mSinglePackageName!!,
+                if (freeze)
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER
+                else
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                0
+            )
+            returnValue = ERROR_NO_ERROR_CAUGHT_UNKNOWN_RESULT
+        }
+        return returnValue
+    }
+
+    private fun pureExecuteAPISystemAppDisabledAction(): Int {
+        if (mSinglePackageName == null) return ERROR_SINGLE_PACKAGE_NAME_IS_NULL
+        if (!isSystemApp(mContext)) return ERROR_NOT_SYSTEM_APP
+        var returnValue = ERROR_OTHER
+        val freeze = mActionMode == ACTION_MODE_FREEZE
+        if ("cf.playhi.freezeyou" != mSinglePackageName) {
+            mContext.packageManager.setApplicationEnabledSetting(
+                mSinglePackageName!!,
+                if (freeze)
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                else
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                0
+            )
+            returnValue = ERROR_NO_ERROR_CAUGHT_UNKNOWN_RESULT
+        }
+        return returnValue
+    }
+
+    companion object {
+        const val ACTION_MODE_FREEZE = 0
+        const val ACTION_MODE_UNFREEZE = 1
+        const val ERROR_NO_ERROR_CAUGHT_UNKNOWN_RESULT = 1
+        const val ERROR_NO_ERROR_SUCCESS = 0
+        const val ERROR_OTHER = -1
+        const val ERROR_SINGLE_PACKAGE_NAME_IS_NULL = -2
+        const val ERROR_DEVICE_ANDROID_VERSION_TOO_LOW = -3
+        const val ERROR_NO_ROOT_PERMISSION = -4
+        const val ERROR_DPM_EXECUTE_FAILED_FROM_SYSTEM = -5
+        const val ERROR_NOT_DEVICE_POLICY_MANAGER = -6
+        const val ERROR_NO_SUCH_API_MODE = -7
+        const val ERROR_NOT_SYSTEM_APP = -8
+
+        /**
+         * 使用 FreezeYou 的 自动（免ROOT(DPM)/ROOT(DISABLE)） 模式
+         * 推荐直接使用 [API_FREEZEYOU_MROOT_DPM]
+         * 与 [API_FREEZEYOU_ROOT_DISABLE_ENABLE]
+         * 与 [API_FREEZEYOU_ROOT_UNHIDE_HIDE]
+         */
+        @Deprecated("old")
+        const val API_FREEZEYOU_LEGACY_AUTO = 0
+
+        /**
+         * 使用 FreezeYou 的 免ROOT(DPM) 模式
+         */
+        const val API_FREEZEYOU_MROOT_DPM = 1
+
+        /**
+         * 使用 FreezeYou 的 ROOT(DISABLE) 模式
+         */
+        const val API_FREEZEYOU_ROOT_DISABLE_ENABLE = 2
+
+        /**
+         * 使用 FreezeYou 的 ROOT(UNHIDE) 模式
+         */
+        const val API_FREEZEYOU_ROOT_UNHIDE_HIDE = 3
+
+        /**
+         * 使用 FreezeYou 的 System App (DISABLE_UNTIL_USED) 模式
+         */
+        const val API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE_UNTIL_USED = 4
+
+        /**
+         * 使用 FreezeYou 的 System App (DISABLE_USER) 模式
+         */
+        const val API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE_USER = 5
+
+        /**
+         * 使用 FreezeYou 的 System App (DISABLE) 模式
+         */
+        const val API_FREEZEYOU_SYSTEM_APP_ENABLE_DISABLE = 6
+    }
+}
